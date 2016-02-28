6070deb532971089e403dec0fe06b6fbe2877681
Chad Rutherford <c.ruth544@gmail.com>
2016-02-27 18:22:22 -0800

    init repo

---
+++ boggle_solver.rb
@@ -0,0 +1,24 @@
+require "pry"
+class Boggle_Char
+  attr_accessor :char
+  attr_accessor :used
+
+  def initialize(char)
+    self.char = char
+  end
+
+  def to_s
+    char
+  end
+
+end
+
+class Boggle_Find
+  def convert arr
+    boggle_array = []
+    arr.each do |obj|
+
+    end
+  end
+
+end
