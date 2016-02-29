fb0beaff051a9a6b9bc5b6b5f3e8e3504dc649d5
Chad Rutherford <c.ruth544@gmail.com>
2016-02-28 20:25:28 -0800

    completes, but not correct answers (not close)

--- boggle_solver.rb
+++ boggle_solver.rb
@@ -38,14 +38,15 @@ class Boggle_Find
 
   def self.find_word board_as_string
     self.board = Boggle_Board.convert(board_as_string)
+    self.english_words = File.read("wordsEn.txt").gsub(/\r/, "")
     self.solution = []
     for i in 0..self.board.length - 1
       row = self.board[i]
       for j in 0..row.length - 1
         char = row[j]
-        regexp = Regexp.new("#{char.char}\w*$")
+        regexp = Regexp.new("#{char.char}\\w+$")
         self.set_possible_words(self.english_words, regexp)
-        self.next_char({row: i, col: j}, char)
+        self.next_char({row: i, col: j}, char.char)
       end
     end
     self.solution
@@ -62,31 +63,31 @@ class Boggle_Find
         next if self.board[i][j].used
         # Check if the next char is an actual index
         next if i == 0 and j == 0
-        next if origin[row] + i < 0 or origin[row] + i >= self.board.length
-        next if origin[col] + j < 0 or origin[col] + j >= self.board.length
+        next if origin[:row] + i < 0 or origin[:row] + i >= self.board.length
+        next if origin[:col] + j < 0 or origin[:col] + j >= self.board.length
 
         # New sub_str to test
         new_sub_str = sub_str << self.board[i][j].char
+        # Immediately check to see if it is a full words
+        if self.possible_words.match(Regexp.new("#{new_sub_str}$"))
+          unless self.solution.include?(new_sub_str)
+            self.solution << new_sub_str
+          end
+        end
+
         # make regex to widdle down possible words
-        regexp = Regexp.new(new_sub_str << "\w*$")
+        regexp = Regexp.new("#{new_sub_str}\\w+$")
         is_possible_word = self.set_possible_words(self.possible_words, regexp)
         # is_possible_word is eqivolent to self.possible_words
 
         # Check if new_sub_str is an actual word
+        # binding.pry
         if is_possible_word
-          # Make Regex for word
-          regexp = Regexp.new(new_sub_str << "$")
-          if regexp.match(is_possible_word)
-            unless self.solution.include?(new_sub_str)
-              self.solution << new_sub_str
-            end
-          end
           # Set boolean on char that has been used
           self.board[i][j].used = true
           # Continue to check if there is another possible word
           self.next_char({row: i, col: j}, new_sub_str)
         end
-
       end
     end
 
@@ -99,8 +100,9 @@ class Boggle_Find
 
   def self.set_possible_words wordbank, regexp
     # Shortens list of possible words
+    # binding.pry
     reg_ex_list = wordbank.scan(regexp)
-    if reg_ex_list
+    if reg_ex_list.length > 0
       # If it is possible, reset possible words list
       self.possible_words = reg_ex_list.join("\n")
     else
@@ -112,10 +114,10 @@ class Boggle_Find
 end
 
 boggle = [
-  ["D", "G", "H", "I"],
+  ["A", "G", "H", "I"],
   ["K", "L", "P", "S"],
   ["Y", "E", "U", "T"],
   ["E", "O", "R", "N"],
 ]
-# board = Boggle_Board.find_word(boggle)
+board = Boggle_Board.convert(boggle)
 # puts board
